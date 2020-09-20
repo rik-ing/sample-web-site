1d45ac2491a2ba17b9af62dcf34e376cc31d8a0c
rik-ing <u.rik.ing0804@gmail.com>
2020-09-20 12:20:02 +0900

    add json elements.

--- src/js/index.js
+++ src/js/index.js
@@ -12,13 +12,39 @@ var sample_json = [
 var tips;
 tips = document.getElementById('tips');
 
+var content;
+
 var card;
-card = document.createElement('dev');
+var title;
+var title_elem;
+var title_elem2;
+var title_header;
+var description;
+var description_text;
 
 for(var i = 0; i < sample_json.length; i++) {
     card = document.createElement('div'); 
     card.setAttribute('class', 'card');
 
+    content = document.createElement('div');
+    content.setAttribute('class', 'content');
+    title = document.createElement('div');
+    title.setAttribute('class', 'title');
+    
+    title_elem = document.createElement('h2');
+    title_elem2 = document.createElement('strong');
+    title_header = document.createTextNode(sample_json[i].title);
+
+    description = document.createElement('div');
+    description.setAttribute('class', 'description');
+    description_text = document.createTextNode(sample_json[i].description);
     tips.appendChild(card);
-    console.log(card);
+    
+    card.appendChild(content);
+    content.appendChild(title);
+    title.appendChild(title_elem);
+    title_elem.appendChild(title_header);
+
+    content.appendChild(description);
+    description.appendChild(description_text);
 }
